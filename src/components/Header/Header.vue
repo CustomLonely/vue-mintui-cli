f3bf9c5cb26d9f378ff5c3205bdeda7aad488254
cz <chenzhen@fengjinggroup.com>
2018-08-27 17:44:11 +0800

    vue-cli 骨架屏 兼容ie9 rem自适应

---
+++ src/components/Header/Header.vue
@@ -0,0 +1,29 @@
+<template>
+  <mt-header :title="mytitle">
+    <mt-button @click="goPage()" icon="back" slot="left"></mt-button>
+  </mt-header>
+</template>
+
+<script>
+export default {
+  props: ["mytitle"],
+  data() {
+    return {};
+  },
+  created() {
+    console.log(this.mytitle);
+  },
+  mounted() {
+    console.log(this.mytitle);
+  },
+  methods: {
+    goPage() {
+      this.$router.go(-1);
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+</style>
---
+++ src/pages/other/Login.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="main">
+    <mt-field label="用户名" placeholder="请输入用户名"></mt-field>
+    <mt-field label="密码" placeholder="请输入密码" type="password">
+      <mt-switch :value.sync="value" name="c..."></mt-switch>
+    </mt-field>
+    <mt-field label="验证码" class="security_code">
+      <img src="../../assets/images/timg.jpg">
+
+    </mt-field>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      value: ""
+    };
+  }
+};
+</script>
+
+<style lang="less">
+.main {
+  margin-top: 15px;
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  .mint-cell {
+    border-bottom: 1px solid #eee;
+    .mint-cell-wrapper {
+      .mint-cell-title {
+        width: 60px;
+      }
+    }
+  }
+  .security_code {
+    display: flex;
+    max-height: 48px;
+    align-items: center;
+    justify-content: space-between;
+    .btns {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+    }
+  }
+}
+</style>
